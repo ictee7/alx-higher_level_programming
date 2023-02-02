a7699fe3293359adffd511c9f8ec87847e36f03f
Ian Chirwa <chirwaian7@gmail.com>
2023-02-02 11:20:10 +0200

    modified 0-add_integer.py file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,3 +20,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     else:
         return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
